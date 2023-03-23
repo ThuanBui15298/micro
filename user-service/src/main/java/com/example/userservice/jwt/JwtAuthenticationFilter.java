dbe9bd4de4426065965c1a50f814c08225795510
vuminhduc2209 <ducvm@thuanthanhtech.com>
2023-03-23 16:50:57 +0700

    fixx

--- user-service/src/main/java/com/example/userservice/jwt/JwtAuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/jwt/JwtAuthenticationFilter.java
@@ -62,7 +62,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     log.error("Error logging in: {}", e.getMessage());
                     response.setHeader("error", e.getMessage());
                     response.setStatus(FORBIDDEN.value());
-                    //response.sendError(FORBIDDEN.value());
                     Map<String, String> error = new HashMap<>();
                     error.put("error", e.getMessage());
                     response.setContentType(APPLICATION_JSON_VALUE);
